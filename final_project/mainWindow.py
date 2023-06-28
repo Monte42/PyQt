62121a49d45a1c719f8f631aa17206fb370d8342
Monte DuMond <gdumond42@gmail.com>
2023-06-28 17:25:01 -0400

    Final Project Menu 'show calc' function working

--- final_project/mainWindow.py
+++ final_project/mainWindow.py
@@ -9,13 +9,21 @@ class MainWindow(QMainWindow):
         super().__init__()
         uic.loadUi('./views/main.ui', self)
 
-        self.addDockWidget(Qt.DockWidgetArea.RightDockWidgetArea, Calculator())
-        with open('css/calculator.css','r') as file:
-            self.layout().itemAt(0).widget().setStyleSheet(file.read())
+        self.openCalculator(True)
 
 
 
         self.enterAdmin.triggered.connect(self.openAdminDashboard)
+        self.showCalculator.triggered.connect(self.openCalculator)
+
+    def openCalculator(self,firstOpen=False):
+        if not firstOpen: self.removeDockWidget(self.layout().itemAt(0).widget())
+        self.addDockWidget(Qt.DockWidgetArea.RightDockWidgetArea, Calculator())
+        with open('css/calculator.css','r') as file:
+            for i in range(self.layout().count()):
+                print(self.layout().itemAt(i).widget())
+            self.layout().itemAt(0).widget().setStyleSheet(file.read())
+        print('\n\n\n')
 
     def openAdminDashboard(self):
         self.adminDashboard = AdminDashboard()
